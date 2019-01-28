3bbc62ccea7101a535c0f00d4ffb901ce91e1c44
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2019-01-28 14:00:40 +0100

    basic classes

---
+++ Actor.php
@@ -0,0 +1,48 @@
+<?php
+
+// Actor.php
+
+class Actor extends Db {
+
+    protected $id;
+    protected $firstname;
+    protected $lastname;
+
+    const TABLE_NAME = "Actor";
+
+    public function __construct() {
+
+    }
+
+    public function id() {
+        return $this->id;
+    }
+
+    public function firstname() {
+        return $this->firstname;
+    }
+
+    public function lastname() {
+        return $this->lastname;
+    }
+
+    public function setfirstname($firstname) {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    public function setlastname($lastname) {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    public function save() {
+        $data = [
+            'firstname' => $this->firstname,
+            'lastname'  => $this->lastname
+        ];
+
+        return $this->dbCreate($data);
+
+    }
+}
---
+++ Movie.php
@@ -0,0 +1,67 @@
+<?php 
+
+// Movie.php
+
+class Movie {
+    
+    protected $id;
+    protected $title;
+    protected $releaseDate;
+    protected $plot;
+    protected $id_category;
+
+    public function __construct() {
+
+    }
+
+    public function id() {
+        return $this->id;
+    }
+
+    public function title() {
+        return $this->title;
+    }
+
+    public function releaseDate() {
+        return $this->releaseDate;
+    }
+
+    public function plot() {
+        return $this->plot;
+    }
+
+    public function idCategory() {
+        return $this->idCategory;
+    }
+    
+    public function category() {
+        // return $this->category->name;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function setReleaseDate($releaseDate) {
+        $this->releaseDate = $releaseDate;
+        return $this;
+    }
+
+    public function setPlot($plot) {
+        $this->plot = $plot;
+        return $this;
+    }
+
+    public function setCategory(Category $category) {
+
+        // $this->category_id = $category->id();
+
+        return $this;
+    }
+
+}
+
+$movie = new Movie();
+
+$movie->title();
